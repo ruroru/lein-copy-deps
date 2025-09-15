ab656f57b894ca688fa036509601ed3320160168
ruroru <111705692+ruroru@users.noreply.github.com>
2025-09-15 20:51:09 +0000

    Stepping project version for the next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.jj/copy-deps  "1.0.2"
+(defproject org.clojars.jj/copy-deps  "1.0.3-SNAPSHOT"
   :description "lein-copy-deps is a Leiningen plugin that copies all your project's dependencies to a single directory. It supports both hard copy and symlink"
   :url "https://github.com/ruroru/lein-copy-deps"
   :license {:name "EPL-2.0"
